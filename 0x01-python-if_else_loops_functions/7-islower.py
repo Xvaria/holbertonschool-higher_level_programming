70eaffdeccea6687fde34064d2fab45239d03379
Xvaria <2349@holbertonschool.com>
2020-12-08 22:21:18 +0000

    Ready checks for lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def islower(c):
     l = ord(c)
-    if l >= 65 and l <= 90:
-        return False
-    elif l >= 97 and l <= 122:
+    if l >= 97 and l <= 122:
         return True
+    else:
+        return False
