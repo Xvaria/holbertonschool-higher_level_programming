3f8d1ed7b6fcfdc796e247e5084949962701dd00
Xvaria <2349@holbertonschool.com>
2020-12-09 15:44:16 +0000

    Correction v2.0 add condition

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    nstr = str[:n] + str[n+1:]
-    return nstr
+    if n >= 0:
+        nstr = str[:n] + str[n+1:]
+        return nstr
+    return str
