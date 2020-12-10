53212e2f8edfe131bbda9692afc09a91cb69622a
Xvaria <2349@holbertonschool.com>
2020-12-10 23:46:26 +0000

    Correction v2.0 create two variables for the numeric arguments and correction pep8 style

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,14 +3,16 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     if len(argv) == 4:
+        a = int(argv[1])
+        b = int(argv[3])
         if argv[2] == "+":
-            print("{} + {} = {}".format(int(argv[1]), int(argv[3]), add(int(argv[1]), int(argv[3]))))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif argv[2] == "-":
-            print("{} + {} = {}".format(int(argv[1]), int(argv[3]), sub(int(argv[1]), int(argv[3]))))
+            print("{} + {} = {}".format(a, b, sub(a, b)))
         elif argv[2] == "*":
-            print("{} + {} = {}".format(int(argv[1]), int(argv[3]), mul(int(argv[1]), int(argv[3]))))
+            print("{} + {} = {}".format(a, b, mul(a, b)))
         elif argv[2] == "/":
-            print("{} + {} = {}".format(int(argv[1]), int(argv[3]), div(int(argv[1]), int(argv[3]))))
+            print("{} + {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
