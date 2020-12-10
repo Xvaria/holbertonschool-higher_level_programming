1c0153f6351598433aba40eb6c3718ceea44a8f5
Xvaria <2349@holbertonschool.com>
2020-12-10 22:18:10 +0000

    Correction v2.0 add new condition for one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     i = 1
-    if len(argv) > i:
+    if len(argv) == 2:
         print("{} argument:".format(len(argv) - 1))
+        print("{}: {}".format(i, argv[i]))
+    elif len(argv) > 2:
+        print("{} arguments:".format(len(argv) - 1))
         while i < len(argv):
             print("{}: {}".format(i, argv[i]))
             i = i + 1
