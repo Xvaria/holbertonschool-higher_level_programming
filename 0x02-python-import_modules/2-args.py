40b1062a963b7bb9f0007a1730b5f6b150b32c51
Xvaria <2349@holbertonschool.com>
2020-12-10 22:01:35 +0000

    Ready prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    i = 1
+    if len(argv) > i:
+        print("{} argument:".format(len(argv) - 1))
+        while i < len(argv):
+            print("{}: {}".format(i, argv[i]))
+            i = i + 1
+        else:
+            print("{} arguments.".format(len(argv) - 1))
