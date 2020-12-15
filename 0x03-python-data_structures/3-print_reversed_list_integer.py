6777efd6581188b7031fec5ee0d00f0346ee47fd
Xvaria <2349@holbertonschool.com>
2020-12-15 18:09:39 +0000

    Correction v2.05 pep8 style

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-   for j in my_list[::-1]:
-      print("{:d}".format(j))
+    for i in my_list[::-1]:
+        print("{:d}".format(i))
