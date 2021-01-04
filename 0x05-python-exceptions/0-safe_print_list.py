fe01233482193c580372219a5be805f02fc292ab
Xvaria <2349@holbertonschool.com>
2021-01-04 20:32:11 +0000

    Ready prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        a = 0
+        for i in range(x):
+            print("{}".format(my_list[i]), end = "")
+            a += 1
+        print()
+        return(a)
+    except IndexError:
+        a = 0
+        for i in my_list:
+            a += 1
+        print()
+        return(a)
