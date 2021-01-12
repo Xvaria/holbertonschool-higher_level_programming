3e6a39dd48b9f23d4492af252220e0453f58291a
Xvaria <2349@holbertonschool.com>
2021-01-12 00:41:27 +0000

    Ready A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,13 +9,12 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         'starts our rectangle with default values'
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def area(self):
         'returns the area of the rectangle'
-        Rectangle.number_of_instances += 1
         a = self.__width * self.__height
         return(a)
 
