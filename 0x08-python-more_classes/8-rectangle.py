51aa2488cf64ef0ecbb6e51cee0906f1e1e1bf67
Xvaria <2349@holbertonschool.com>
2021-01-12 00:57:48 +0000

    Ready Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         'starts our rectangle with default values'
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def area(self):
