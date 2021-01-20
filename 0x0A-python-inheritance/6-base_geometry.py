5983e190ad9d9494bce99e4aaa9eab862b790958
Xvaria <2349@holbertonschool.com>
2021-01-20 04:34:11 +0000

    Ready Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'Geometry module'
+
+
+class BaseGeometry():
+    'class BaseGeometry'
+    def area(self):
+        'Public instance method'
+        raise Exception("area() is not implemented")
