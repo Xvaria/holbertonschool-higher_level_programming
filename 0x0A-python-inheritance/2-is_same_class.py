b97a63514020f33d5312b155a4db1afafeadfc66
Xvaria <2349@holbertonschool.com>
2021-01-20 04:39:03 +0000

    Ready Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'is_same_class'
+
+
 def is_same_class(obj, a_class):
     'returns True if the object is exactly an instance of the specified class'
     if type(obj) == a_class:
