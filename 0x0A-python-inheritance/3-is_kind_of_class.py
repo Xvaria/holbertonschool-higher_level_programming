090a8a33d9bd124b4df3b6e40ea787ee9f448fd3
Xvaria <2349@holbertonschool.com>
2021-01-20 04:40:59 +0000

    Ready Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'Same class or inherit from'
+
+
 def is_kind_of_class(obj, a_class):
     'turns True if the object is an instance of, or if the object is an\
      instance of a class that inherited from, the specified class'
