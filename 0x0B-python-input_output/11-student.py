7ee8f3be33906d874f95fb83fc90e6e12689102e
Xvaria <2349@holbertonschool.com>
2021-01-21 06:11:11 +0000

    Ready Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'Student to JSON'
+
+
+class Student():
+    'defines a student'
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        'retrieves a dictionary representation of a Student instance'
+        if type(attrs) != list:
+            return(self.__dict__)
+        t = {}
+        for i in attrs:
+            if type(i) != str:
+                return(self.__dict__)
+            if i in self.__dict__.keys():
+                t[i] = self.__dict__[i]
+        return(t)
+
+    def reload_from_json(self, json):
+        'replaces all attributes of the Student instance'
+        for j in json.keys():
+            self.__dict__[j] = json[j]
