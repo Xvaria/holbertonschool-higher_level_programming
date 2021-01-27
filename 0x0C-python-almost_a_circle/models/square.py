623ad5c72f56516f0e099e65e50a63333d82f07c
Xvaria <2349@holbertonschool.com>
2021-01-27 14:52:20 +0000

    Correction v2.0 pep8 and add coments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,20 +4,20 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    ''
+    'inherits from Rectangle'
 
     def __init__(self, size, x=0, y=0, id=None):
-        ''
+        'Private instance attributes'
         super().__init__(width=size, height=size, x=x, y=y, id=id)
         self.size = size
 
     def __str__(self):
-        ''
+        'return square properties'
         return('[{}] ({}) {}/{} - {}'.format(self.__class__.__name__, self.id,
                                              self.x, self.y, self.__size))
 
     def update(self, *args, **kwargs):
-        ''
+        'assigns attributes'
         for i, j in kwargs.items():
             setattr(self, i, j)
         try:
@@ -29,14 +29,15 @@ class Square(Rectangle):
             pass
 
     def to_dictionary(self):
-        ''
-        return({'id':self.id, 'size':self.__size, 'x':self.x, 'y':self.y})
+        'returns the dictionary representation of a Square'
+        return({'id': self.id, 'size': self.__size, 'x': self.x, 'y': self.y})
 
     @property
     def size(self):
+        'return the rectangle size'
         return(self.__size)
 
     @size.setter
     def size(self, value):
-        ''
+        'assign the same value to width and the height'
         self.__size = value
