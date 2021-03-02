15812fab4c9a3cae7ba9278f82eecbe96d066fa7
Xvaria <2349@holbertonschool.com>
2021-03-02 21:28:49 +0000

    Ready Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@
 -- Creates the user user_0d_2, If the user already exists, script not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2 . * TO 'user_0d_2'@'localhost';
