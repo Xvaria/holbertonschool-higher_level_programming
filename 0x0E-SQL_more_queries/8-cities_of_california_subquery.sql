af8a8b49f51bb3b4a23fbdb0ffe3db4e032384aa
Xvaria <2349@holbertonschool.com>
2021-03-03 04:51:45 +0000

    Ready Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Cities of California
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 
-SELECT * FROM cities WHERE state_id = 1 ORDER BY id DESC;
+SELECT cities.id, cities.name FROM cities WHERE state_id = 1 ORDER BY id DESC;
