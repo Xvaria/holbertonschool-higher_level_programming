84345ac8c3d89fb077ac9cd872031145d1baf01c
Xvaria <2349@holbertonschool.com>
2021-03-22 17:29:29 +0000

    Correction v2.0 added a new conditional

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,9 +3,13 @@
 // prints My number: <first argument converted in integer> if the first argument can be converted to an integer
 
 const myArgs = process.argv;
-const newArgs = parseInt(myArgs[2]);
-if (isNaN(newArgs)) {
+if (myArgs.length !== 3) {
   console.log('Not a number');
 } else {
-  console.log(newArgs);
+  const newArgs = parseInt(myArgs[2]);
+    if (isNaN(newArgs)) {
+      console.log('Not a number');
+    } else {
+      console.log(newArgs);
+    }
 }
