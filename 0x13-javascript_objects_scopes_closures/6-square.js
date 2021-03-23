406435207a016d087b789e21e71798f3e0d4e053
Xvaria <2349@holbertonschool.com>
2021-03-23 22:19:43 +0000

    Ready Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+    let i = 0;
+    while (i < this.width) {
+      console.log(c.repeat(this.width));
+      i++;
+    }
+  }
+}
+
+module.exports = Square;
