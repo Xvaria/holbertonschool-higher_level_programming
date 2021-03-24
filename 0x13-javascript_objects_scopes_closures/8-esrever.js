82902bf6f672f1b8a12623c32134f110f0d3e62b
Xvaria <2349@holbertonschool.com>
2021-03-24 00:50:26 +0000

    Ready Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 // returns the reversed version of a list
 
 exports.esrever = function (list) {
-    let array = [];
-    list.forEach (item => array.unshift(item));
-    return (array);
+  const array = [];
+  list.forEach(item => array.unshift(item));
+  return (array);
 };
