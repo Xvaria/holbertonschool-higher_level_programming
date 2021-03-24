44daf71558439138ed36ee156b81dcf38d94c36e
Xvaria <2349@holbertonschool.com>
2021-03-24 00:13:52 +0000

    ready Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// returns the number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let noo = 0;
+  list.forEach(item => {
+    if (item === searchElement) {
+      noo++;
+    }
+  });
+  return (noo);
+}
