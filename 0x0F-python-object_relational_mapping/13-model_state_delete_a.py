5ace7231dbc0ff4de196ac656f8505d48e801481
Xvaria <2349@holbertonschool.com>
2021-03-28 09:42:24 +0000

    Ready Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     enter = sessionmaker()(bind=engine)
-    query = enter.query(State)
+    query = enter.query(State).order_by(State.id)
     for item in query:
         if 'a' in item.name:
             query1 = enter.query(State).filter_by(name=item.name)
