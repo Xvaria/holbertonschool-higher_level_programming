2f0db27493852cda2c78e9cfffbd81d323a0a4d9
Xvaria <2349@holbertonschool.com>
2021-03-28 19:03:07 +0000

    Ready City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''contains the class definition of a City'''
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    '''class definition of a State'''
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
