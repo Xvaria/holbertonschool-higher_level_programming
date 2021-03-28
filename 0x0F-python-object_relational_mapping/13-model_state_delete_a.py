c087e9385b8dbc8a73c75bcebf7a31b2af0b101c
Xvaria <2349@holbertonschool.com>
2021-03-28 09:47:40 +0000

    Correction v2.0 come back my first sintax

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,9 +12,7 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     enter = sessionmaker()(bind=engine)
-    query = enter.query(State)
+    query = enter.query(State).filter(State.name.like('%a%'))
     for item in query:
-        if 'a' in item.name:
-            query1 = enter.query(State).filter_by(name=item.name)
-            query1.delete()
-    enter.commit()
+        enter.delete(item)
+        enter.commit()
