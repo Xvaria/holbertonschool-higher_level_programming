05a78011607ed55c71e8ac032dbcb80f7fad1376
Xvaria <2349@holbertonschool.com>
2021-03-28 19:02:52 +0000

    Ready City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''contains the class definition of a State and an\
+   instance Base = declarative_base()'''
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''class definition of a State'''
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="states",
+                            cascade="all, delete-orphan")
