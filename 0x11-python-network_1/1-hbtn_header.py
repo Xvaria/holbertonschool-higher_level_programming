d846b5471c7ed9486ff1d325af51225df71f7ee8
Xvaria <2349@holbertonschool.com>
2021-04-09 23:02:24 +0000

    Ready | Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
 import urllib.request
 from sys import argv
 
-
-with urllib.request.urlopen(argv[1]) as response:
-    html = response.info().get("X-Request-Id")
-    print("{}".format(html))
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        html = response.info().get("X-Request-Id")
+        print("{}".format(html))
