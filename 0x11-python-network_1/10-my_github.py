56690a2ceb443b75a2398fa5ff9e1a4e58f09891
Xvaria <2349@holbertonschool.com>
2021-04-10 03:36:39 +0000

    Ready | My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''fetches https://intranet.hbtn.io/status'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    html = requests.get("https://api.github.com/user",
+                         auth=requests.auth.HTTPBasicAuth(argv[1], argv[2]))
+    json = html.json()
+    print("{}".format(json.get('id')))
