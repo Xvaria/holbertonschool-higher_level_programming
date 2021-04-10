df64c3107601b83b646db68133731753c4c10391
Xvaria <2349@holbertonschool.com>
2021-04-10 01:58:21 +0000

    Ready | Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,9 +4,10 @@ import urllib.request
 from sys import argv
 
 
-try:
-    with urllib.request.urlopen(argv[1]) as response:
-        html = response.read()
-        print("{}".format(html.decode('utf-8')))
-except urllib.error.HTTPError as error:
-    print("Error code: {}".format(error.code))
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            html = response.read()
+            print("{}".format(html.decode('utf-8')))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
