aa069d9ac68107e911c9a7b4b12545e5fa80dad4
Xvaria <2349@holbertonschool.com>
2021-04-11 15:44:24 +0000

    Ready | List 10 commits to use the GitHub API

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''takes 2 arguments in order to solve this challenge'''
+'''list 10 commits of the repository rails by the user rails'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    html = requests.get("https://api.github.com/repos/{}/{}/commits".
+                        format(argv[2], argv[1]))
+    json = html.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(json[i].get('sha'), json[i].get('commit').
+                                  get('author').get('name')))
+    except:
+        pass
